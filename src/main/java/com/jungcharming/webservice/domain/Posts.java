236250553d42c4e3f72f8f99cf527588d9a0b249
cheol hee <wjdcjf1229@gmail.com>
2019-03-28 20:39:43 +0900

    main open

--- src/main/java/com/jungcharming/webservice/domain/Posts.java
+++ src/main/java/com/jungcharming/webservice/domain/Posts.java
@@ -1,11 +1,15 @@
 package com.jungcharming.webservice.domain;
 
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
--- src/main/java/com/jungcharming/webservice/dto/posts/PostsSaveRequestDto.java
+++ src/main/java/com/jungcharming/webservice/dto/posts/PostsSaveRequestDto.java
@@ -1,20 +1,24 @@
 package com.jungcharming.webservice.dto.posts;
 
+
 import com.jungcharming.webservice.domain.Posts;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class PostsSaveRequestDto {
+
     private String title;
     private String content;
     private String author;
 
     public Posts toEntity(){
-        return Posts.builder().title(title)
+        return Posts.builder()
+                .title(title)
                 .content(content)
                 .author(author)
                 .build();
